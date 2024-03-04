57872fd6d9b55953895fa2636a931e4c46b3fac9
muyeon <gmakin36@gmail.com>
2024-03-04 16:01:36 +0900

    init: setup user
    유저 모듈, 유저 컨트롤러, 유저 서비스 setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [__dirname + '/**/*.entity{.ts,.js}'], // 나중에 생성할 모델들을 넣는 곳, entitiy 클래스의 경로를 지정
       synchronize: process.env.DB_SYNCHRONIZE === 'true',
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
