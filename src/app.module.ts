60a3a0f4c56c659e85023107aa0a8f3bd2187623
muyeon <gmakin36@gmail.com>
2024-03-04 15:54:59 +0900

    feat: connect database
    TypeORM 데이터베이스 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: ~~process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // 나중에 생성할 모델들을 넣는 곳, entitiy 클래스의 경로를 지정
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
