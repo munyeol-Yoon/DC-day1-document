8464c0059aebd49683a1e3b5a0e3077843a0bc85
muyeon <gmakin36@gmail.com>
2024-03-04 21:21:12 +0900

    feat: add create JWT access_token
    로그인 JWT access_token 생성

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/user/entity/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forFeature([UserEntity]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,15 +3,17 @@ import { LoginRequestDto } from './dto/login.req.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from 'src/user/entity/user.entity';
 import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    private jwtService: JwtService,
   ) {}
 
-  async signIn(body: LoginRequestDto): Promise<any> {
+  async signIn(body: LoginRequestDto): Promise<{ access_token: string }> {
     const user = await this.userRepository.findOne({
       where: {
         email: body.email,
@@ -23,10 +25,12 @@ export class AuthService {
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { password, ...result } = user;
+    // const { password, ...result } = user;
 
-    // TODO JWT 생성하고 반환
+    const payload = { sub: user.id, name: user.name };
 
-    return result;
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
