78ab6399fc296494a0e5a8e3c5056be2f1ce1100
muyeon <gmakin36@gmail.com>
2024-03-04 20:49:48 +0900

    init: auth setup
    인증 인가를 연습하기 위한 auth module, controller ,service 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { UserModule } from './user/user.module';
       synchronize: process.env.DB_SYNCHRONIZE === 'true',
     }),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
