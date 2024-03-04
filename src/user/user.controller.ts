3fc6a7dca3403c376901c1c05b736872b3cbb232
muyeon <gmakin36@gmail.com>
2024-03-04 16:54:26 +0900

    refactor: Response value excluding password
    응답값 비밀번호 제외, 객체를 생성해 password 만 제외해 반환하는 방법을 썼으나 이 부분은 효율적으로 작성하도록 고민해야함. 추후 변경

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserRequestDto } from './dto/user.request.dto';
+import { UserResponseDto } from './dto/user.response.dto';
 
 @Controller('api')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('/signup')
-  async createUser(@Body() body: UserRequestDto) {
+  async createUser(@Body() body: UserRequestDto): Promise<UserResponseDto> {
     return this.userService.createUser(body);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user.entity';
 import { Repository } from 'typeorm';
 import { UserRequestDto } from './dto/user.request.dto';
+import { UserResponseDto } from './dto/user.response.dto';
 
 @Injectable()
 export class UserService {
@@ -11,11 +12,19 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>,
   ) {}
 
-  async createUser(body: UserRequestDto) {
+  async createUser(body: UserRequestDto): Promise<UserResponseDto> {
     const user = await this.userRepository.create({
       ...body,
     });
 
-    return await this.userRepository.save(user);
+    const saveUser = await this.userRepository.save(user);
+
+    const result: UserResponseDto = {
+      id: saveUser.id,
+      name: saveUser.name,
+      email: saveUser.email,
+    };
+
+    return result;
   }
 }
