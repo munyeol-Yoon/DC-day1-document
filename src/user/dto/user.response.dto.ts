f6171b67725b7278d6100851b4fa4781b0552dce
muyeon <gmakin36@gmail.com>
2024-03-04 17:21:22 +0900

    feat: add validation pipe
    class-validator 를 이용한 요청 응답 pipe 검증 데코레이터 추가

--- src/user/dto/user.response.dto.ts
+++ src/user/dto/user.response.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class UserResponseDto {
+  @IsString()
+  @IsNotEmpty()
   id: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 }
